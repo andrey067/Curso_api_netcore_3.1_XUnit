02d8ac24c0d199f4b58a9cb65868f085ce976c24
andrey067 <audrey.ernesto.lima@gmail.com>
2023-06-25 20:23:22 -0400

    removendo DbConnection

--- Infrastructure/Context/ContextFactory.cs
+++ Infrastructure/Context/ContextFactory.cs
@@ -7,7 +7,7 @@ namespace Infrastructure.Context
     {
         public EnderecosContext CreateDbContext(string[] args)
         {
-            var connectionString = "Host=192.168.237.70; Database=CursoApi; Username=postgres; Password=admin";
+            var connectionString = "";
             var optionsBuilder = new DbContextOptionsBuilder<EnderecosContext>();
             optionsBuilder.UseNpgsql(connectionString);
             return new EnderecosContext(optionsBuilder.Options);
